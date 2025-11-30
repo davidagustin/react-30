9535205c6a57905bb723f4dec745733f7ab17a7c
dagustin415 <30575095+davidagustin@users.noreply.github.com>
2025-11-29 17:51:30 -0800

    fix: replace useMemo with useEffect for data fetching in type-ahead component

--- components/projects/type-ahead.tsx
+++ components/projects/type-ahead.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
@@ -15,7 +15,7 @@ export default function TypeAhead() {
   const [searchTerm, setSearchTerm] = useState("");
   const [cities, setCities] = useState<City[]>([]);
 
-  useMemo(() => {
+  useEffect(() => {
     fetch(endpoint)
       .then((res) => res.json())
       .then((data: City[]) => setCities(data))
